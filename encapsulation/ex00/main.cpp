ae4d6603bdad3b4ca82235504a796ab5462750c1
VLN37 <jfpr1237@gmail.com>
2024-08-25 12:33:22 -0300

    m00ex00 tests

--- encapsulation/ex00/main.cpp
+++ encapsulation/ex00/main.cpp
@@ -13,6 +13,21 @@ int main(void) {
     bank.create_account(100);
 
     assert(bank.audit_liquidity() == 5);
+    assert(bank.loan(5) == 5);
+    assert(bank.audit_liquidity() == 0);
+
+    bank.deposit(1, 50);
+    assert(bank[1].get_value() == 47.5);
+    bank.withdraw(1, 47.5);
+    assert(bank[1].get_value() == 0);
+    bank.delete_account(1);
+
+    try {
+        bank.delete_account(1);
+        assert(EXPECTS_THROW == true);
+    } catch (Bank::Exception& e) {
+        assert(strcmp(e.what(), BANK_ACCOUNT_NOT_FOUND) == 0);
+    }
 
     try {
         bank.withdraw(3, 50);
@@ -21,7 +36,13 @@ int main(void) {
         assert(strcmp(e.what(), BANK_ACCOUNT_NOT_FOUND) == 0);
     }
 
-    std::cout << "Bank : " << std::endl;
+    try {
+        bank.loan(5);
+        assert(EXPECTS_THROW == true);
+    } catch (Bank::Exception& e) {
+        assert(strcmp(e.what(), BANK_LIQUIDITY) == 0);
+    }
+
     std::cout << bank << std::endl;
 
     return 0;
